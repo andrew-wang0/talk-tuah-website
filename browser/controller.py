fc51b29c2690ec6561041d163d68c65c058f3a91
Andrew Wang <awwang3@uci.edu>
2025-01-24 22:40:29 -0800

    selenium stuff

--- browser/controller.py
+++ browser/controller.py
@@ -1,15 +1,44 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 
 class BrowserController:
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        headless_options = Options()
+        headless_options.add_argument("--window-size=1920,1080")
+        headless_options.add_argument("--headless")
+        headless_options.add_argument("--disable-gpu")
+        self.headless_driver = webdriver.Chrome(options=headless_options)
+
+        options = Options()
+        options.add_argument("--window-size=1920,1080")
+        self.driver = webdriver.Chrome(options=options)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.headless_driver.quit()
         self.driver.quit()
 
-    def get(self, url):
-        self.driver.get(url)
+    def get(self, url: str):
+        self.headless_driver.get(url)
 
     def html(self):
-        return self.driver.page_source
+        return self.headless_driver.page_source
 
+    def screenshot(self, path: str = "./tmp/screenshot.png"):
+        total_height = self.headless_driver.execute_script("return document.body.parentNode.scrollHeight")
+        self.headless_driver.set_window_size(1920, total_height)
+        self.headless_driver.save_screenshot(path)
+        base64_img = self.headless_driver.get_screenshot_as_base64()
+        self.headless_driver.set_window_size(1920, 1080)
+
+        return base64_img
+
+    def scroll_to(self, by: By, value: str):
+        scroll = "arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });"
+
+        element = self.headless_driver.find_element(by, value)
+        self.headless_driver.execute_script(scroll, element)
+
+        element = self.driver.find_element(by, value)
+        self.driver.execute_script(scroll, element)
--- browser/test.py
+++ browser/test.py
@@ -1,7 +1,15 @@
 from browser.controller import BrowserController
+from selenium.webdriver.common.by import By
 
-controller = BrowserController()
-controller.get("https://ics.uci.edu/")
-print(controller.html())
+def main():
+    controller = BrowserController()
+    controller.get("https://ics.uci.edu/")
+    print(controller.html())
 
-input()
+    print(controller.screenshot())
+    controller.scroll_to(By.XPATH, "/html/body/div[1]/main/div/section[5]/div[2]/div[1]/div/div/div[2]/div/a")
+
+    input()
+
+if __name__ == "__main__":
+    main()
