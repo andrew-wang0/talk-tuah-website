5ce4f7df95c1b2292278f9793063538273b2f65d
Kevin Wu <kevinwu098@gmail.com>
2025-01-25 10:15:09 -0800

    style: playground readability

--- playground/src/components/chat/ChatMessage.tsx
+++ playground/src/components/chat/ChatMessage.tsx
@@ -1,38 +1,40 @@
 type ChatMessageProps = {
-  message: string;
-  accentColor: string;
-  name: string;
-  isSelf: boolean;
-  hideName?: boolean;
+    message: string;
+    accentColor: string;
+    name: string;
+    isSelf: boolean;
+    hideName?: boolean;
 };
 
 export const ChatMessage = ({
-  name,
-  message,
-  accentColor,
-  isSelf,
-  hideName,
+    name,
+    message,
+    accentColor,
+    isSelf,
+    hideName,
 }: ChatMessageProps) => {
-  return (
-    <div className={`flex flex-col gap-1 ${hideName ? "pt-0" : "pt-6"}`}>
-      {!hideName && (
-        <div
-          className={`text-${
-            isSelf ? "gray-700" : accentColor + "-800 text-ts-" + accentColor
-          } uppercase text-xs`}
-        >
-          {name}
+    return (
+        <div className={`flex flex-col gap-1 ${hideName ? "pt-0" : "pt-6"}`}>
+            {!hideName && (
+                <div
+                    className={`text-${
+                        isSelf
+                            ? "gray-700"
+                            : accentColor + "-800 text-ts-" + accentColor
+                    } uppercase text-base`}
+                >
+                    {name}
+                </div>
+            )}
+            <div
+                className={`pr-4 text-${
+                    isSelf ? "gray-300" : accentColor + "-500"
+                } text-lg leading-snug ${
+                    isSelf ? "" : "drop-shadow-" + accentColor
+                } whitespace-pre-line`}
+            >
+                {message}
+            </div>
         </div>
-      )}
-      <div
-        className={`pr-4 text-${
-          isSelf ? "gray-300" : accentColor + "-500"
-        } text-sm ${
-          isSelf ? "" : "drop-shadow-" + accentColor
-        } whitespace-pre-line`}
-      >
-        {message}
-      </div>
-    </div>
-  );
+    );
 };
